2e6c66499121df0d216c4dcde9a12c21ff9707fa
Taisei Omine <ominetaisei@gmail.com>
2023-08-04 17:38:42 +0900

    changes

---
+++ config.py
@@ -0,0 +1,22 @@
+x=0; y=0; z=0
+config = {
+    "max_watt": 1400,
+    "lights": {
+        0: {
+            # "position": (x, y, z),
+            "watt": 120,
+        },
+        1: {
+            "position": (x, y, z),
+            "watt": 120,
+        },
+        2: {
+            "position": (x, y, z),
+            "watt": 120,
+        },
+        3: {
+            "position": (x, y, z),
+            "watt": 120,
+        },
+    }
+}
--- dmxout.py
+++ dmxout.py
@@ -1,12 +1,24 @@
 import asyncio
 from pyartnet import ArtNetNode
+from config import config
+from random import randint
+
+def check_watt(config, intensities):
+    watt_sum = 0
+    for light, intensity in zip(config["light"], intensities):
+        watt_sum += light["watt"] * intensity
+    
+    if watt_sum <= config["max_watt"]:
+        return True
+    else:
+        return False
 
 async def main():
     # Run this code in your async function
     node = ArtNetNode('127.0.0.1', 6454)
 
     # Create universe 0
-    universe = node.add_universe(1)
+    universe = node.add_universe(0)
 
     # Add a channel to the universe which consists of 3 values
     # Default size of a value is 8Bit (0..255) so this would fill
@@ -15,9 +27,10 @@ async def main():
 
     # Fade channel to 255,0,0 in 5s
     # The fade will automatically run in the background
-    channel.add_fade([255,0,0], 1000)
+    channel.set_fade([randint(0,255),randint(0,255),randint(0,255)], 100)
+    # channel.set_values([randint(0,255),randint(0,255),randint(0,255)])
 
     # this can be used to wait till the fade is complete
     await channel
-
-asyncio.run(main())
+for _ in range(50):
+    asyncio.run(main())
